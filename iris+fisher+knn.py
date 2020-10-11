d6dbf6f6b230463eab144e9ab360265185452ead
YiYang56 <58210317+YiYang56@users.noreply.github.com>
2020-10-11 16:53:09 +0800

    Update iris+fisher+knn.py

--- iris+fisher+knn.py
+++ iris+fisher+knn.py
@@ -1,8 +1,7 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 
-# 定义fisher函数，输入为两个类别的集合、维度和测试机所属的类别，输出为w*和阈值w0
+# 定义fisher函数，输入为所有类别的集合、全局均值、维度和测试集所属的类别，输出为w*和阈值w0
 def fisher(A,B,C,m,n,flag):
     m1 = np.mean(A, axis=0)
     m2 = np.mean(B, axis=0)
@@ -76,10 +75,10 @@ def fisher(A,B,C,m,n,flag):
 
     return w
 
-# 定义knn函数，输入为预处理后的数据集和k值，输出为OA和三个AA
-def knn(iris,k):
+# 定义knn函数，输入为预处理后的数据集、测试集的索引、k值和fisher中求得的投影方向，输出为OA和三个AA
+def knn(iris,g,k,w):
     I = np.zeros((150, 2))
-
+    # 根据w对输入进行投影
     for i in range(150):
         I[i, 0] = np.dot(iris[i], w)
         if i < 50:
@@ -89,42 +88,43 @@ def knn(iris,k):
         else:
             I[i, 1] = 3
 
-    accuracy = 0
-    a1,a2,a3=0,0,0
-    for i in range(150):
-        count1 = 0
-        count2 = 0
-        count3 = 0
-        prediction = 0
-        test = I[i]
-        train = np.delete(I, i, axis=0)
-        distance = np.zeros((149, 2))
-        for t in range(149):
-            distance[t, 1] = np.linalg.norm(test[0] - train[t, 0])
-            distance[t, 0] = train[t, 1]
-        order = distance[np.lexsort(distance.T)]
-        for n in range(k):
-            if order[n, 0] == 1:
-                count1 += 1
-            if order[n, 0] == 2:
-                count2 += 1
-            if order[n, 0] == 3:
-                count3 += 1
-        if count1 >= count2 and count1 >= count3:
-            prediction = 1
-        if count2 >= count1 and count2 >= count3:
-            prediction = 2
-        if count3 >= count1 and count3 >= count2:
-            prediction = 3  # 取出现次数最多的为预测值
-        if prediction == test[1]:
-            accuracy += 1
-            if i<50:
-                a1+=1
-            elif i<100:
-                a2+=1
-            else:
-                a3+=1
-    return accuracy/150,a1/50,a2/50,a3/50
+    # 定义全局变量，以计算分类正确个数
+    global accuracy,a1,a2,a3
+
+    count1 = 0
+    count2 = 0
+    count3 = 0
+    prediction = 0
+    test = I[g]
+    train = np.delete(I, g, axis=0)
+    distance = np.zeros((149, 2))
+    # 计算距离信息，并将其和对应的标签存入distance数组，最后从大到小进行排序
+    for t in range(149):
+        distance[t, 1] = np.linalg.norm(test[0] - train[t, 0])
+        distance[t, 0] = train[t, 1]
+    order = distance[np.lexsort(distance.T)]
+    # 计算前k个最近样本的类别，并统计出现次数最多的类别，取出现次数最多的为预测值
+    for n in range(k):
+        if order[n, 0] == 1:
+            count1 += 1
+        if order[n, 0] == 2:
+            count2 += 1
+        if order[n, 0] == 3:
+            count3 += 1
+    if count1 >= count2 and count1 >= count3:
+        prediction = 1
+    if count2 >= count1 and count2 >= count3:
+        prediction = 2
+    if count3 >= count1 and count3 >= count2:
+        prediction = 3
+    if prediction == test[1]:
+        accuracy += 1
+        if g<50:
+            a1+=1
+        elif g<100:
+            a2+=1
+        else:
+            a3+=1
 
 path=r'iris.data'
 gakki = pd.read_csv(path, header=None)
@@ -133,9 +133,32 @@ iris=gakki.values[0:150,0:4]
 iris1=gakki.values[0:50,0:4]
 iris2=gakki.values[50:100,0:4]
 iris3=gakki.values[100:150,0:4]
-m = np.mean(iris, axis=0)
-w=fisher(iris1,iris2,iris3,m,4,1)
-k=9
-OA,AA1,AA2,AA3=knn(iris,k)
-print('总体精度OA为：{}'.format(OA))
-print('平均精度AA为：{}'.format((AA1+AA2+AA3)/3))
+
+accuracy,a1,a2,a3=0,0,0,0
+
+# 采用留一法
+for i in range(150):
+    if i<50:
+        test=iris1[i]
+        A=np.delete(iris1, i, axis=0)
+        B=iris2
+        C=iris3
+        flag=1
+    elif i<100:
+        test=iris2[i-50]
+        B = np.delete(iris2, i-50, axis=0)
+        A=iris1
+        C=iris3
+        flag=2
+    else:
+        test=iris3[i-100]
+        C = np.delete(iris3, i-100, axis=0)
+        A = iris1
+        B = iris2
+        flag=3
+    m = np.mean(np.delete(iris, i, axis=0), axis=0)
+    w = fisher(A, B, C, m, 4, flag)
+    k=5
+    knn(iris,i,k,w)
+print('总体精度OA为：{}'.format(accuracy/150))
+print('平均精度AA为：{}'.format((a1/50+a2/50+a3/50)/3))
